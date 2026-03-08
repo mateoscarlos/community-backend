98ee2f31049bb0928595bd0069524491d6320e78
Carlos Perez <carlospe.mateos@gmail.com>
2026-03-08 18:30:26 +0100

    refactor: remove default value for CORSAllowedOrigins in config

--- internal/config/config.go
+++ internal/config/config.go
@@ -10,7 +10,7 @@ type Config struct {
 	// Server
 	Port               string `envconfig:"PORT" default:"8080"`
 	Env                string `envconfig:"ENV" default:"local"` // local | dev | prod
-	CORSAllowedOrigins string `envconfig:"CORS_ALLOWED_ORIGINS" default:"http://localhost:5173"`
+	CORSAllowedOrigins string `envconfig:"CORS_ALLOWED_ORIGINS"`
 
 	// Logging
 	LogLevel     string `envconfig:"LOG_LEVEL" default:"info"` // debug | info | warn | error
